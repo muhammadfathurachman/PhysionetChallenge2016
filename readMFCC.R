1999dd37d9b7a35a96a55a95b75e2a3ea40a5616
muhammadfathurachman <thalhah.muhammad@gmail.com>
2016-10-26 06:53:53 -0700

    Add Dataset File

--- readMFCC.R
+++ readMFCC.R
@@ -3,3 +3,5 @@
 library(foreign) # Import function from foreign Library to read arff file
 setwd("H:/THESIS/DATA/MATLAB_CODE")
 dataset <- read.arff("H:/THESIS/DATA/jMIR_2_4_developer/jMIR_2_4_developer/jAudio/feature_values_1.xml")
+
+write.csv(x = dataset,file = "Dataset.csv" )
