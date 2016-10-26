b4c20e657252048fe3454f6fea871c45c17f47d7
muhammadfathurachman <thalhah.muhammad@gmail.com>
2016-10-26 06:47:08 -0700

    Add Read MFCC from R

--- loadData.m
+++ loadData.m
@@ -1,1 +1,24 @@
-dirA = 'H:\\THESIS\\DATA\\training-a\*.wav';
+function spectro = loadData(dirA)
+main = dirA;
+file = '*.wav';
+%dirA = 'H:\\THESIS\\DATA\\training-a\';
+%dira = 'H:\\THESIS\\DATA\\training-a\\';
+namaFile = readData(strcat(main,file));
+%clear dirA;
+
+% Read Audio File in matlab
+[r c] = size(namaFile);
+spectro = {};
+duration = 10;
+
+for i = 1:c
+    info = audioinfo(strcat(dirA,namaFile{i}));
+    time = info.Duration;
+    sample =[1,2*info.SampleRate];
+    [x fs] =  audioread(strcat(main,namaFile{i}),sample); 
+    spectro{i} = spectrogram(x,512);
+end
+clear x fs sample time info i duration r c namaFile;
+end
+
+
---
+++ readMFCC.R
@@ -0,0 +1,5 @@
+
+## Read XML File ARR
+library(foreign) # Import function from foreign Library to read arff file
+setwd("H:/THESIS/DATA/MATLAB_CODE")
+dataset <- read.arff("H:/THESIS/DATA/jMIR_2_4_developer/jMIR_2_4_developer/jAudio/feature_values_1.xml")
