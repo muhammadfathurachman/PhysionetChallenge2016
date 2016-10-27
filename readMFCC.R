7ebce8bf993ce16b647fda5b50b83def39c34de8
muhammadfathurachman <thalhah.muhammad@gmail.com>
2016-10-26 21:39:50 -0700

    Data Preparation

--- readMFCC.R
+++ readMFCC.R
@@ -1,7 +1,7 @@
 
 ## Read XML File ARR
 library(foreign) # Import function from foreign Library to read arff file
-setwd("H:/THESIS/DATA/MATLAB_CODE")
+setwd("H:/THESIS/DATA/MATLAB_CODE/TRAINING_MFCC_W512")
 dataset <- read.arff("H:/THESIS/DATA/jMIR_2_4_developer/jMIR_2_4_developer/jAudio/feature_values_1.xml")
 
-write.csv(x = dataset,file = "Dataset.csv" )
+write.csv(x = dataset,file = "TRAINING_c.csv" )
